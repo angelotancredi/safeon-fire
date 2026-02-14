1c757494908c78bd1079207a364279c19d281be8
angelotancredi <nasix1926@gmail.com>
2026-02-14 12:30:41 +0900

    fix: debug pusher module & env vars in rooms.js

--- api/pusher/rooms.js
+++ api/pusher/rooms.js
@@ -1,6 +1,9 @@
+import Pusher from "pusher";
+
 export default async function handler(req, res) {
     return res.status(200).json({
         ok: true,
-        step: "rooms.js reached"
+        step: "pusher import ok",
+        hasKey: !!process.env.PUSHER_KEY
     });
 }
